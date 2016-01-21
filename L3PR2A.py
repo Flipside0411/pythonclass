3d01568533e52f8e325d3b91ece311c42e27b208
Flipside0411 <hardypch@gmail.com>
2016-01-21 14:21:37 -0400

    updated

---
+++ L3PR2A.py
@@ -0,0 +1,19 @@
+# L3 Problem 2a (2 points possible)
+#
+#ESTIMATED TIME TO COMPLETE: 5 minutes
+
+#In this problem you'll be given a chance to practice writing some while loops.
+
+#1. Convert the following into code that uses a while loop.
+
+#print 2
+#print 4
+#print 6
+#print 8
+#print 10
+#print Goodbye!
+x=2
+while x<10:
+    print x
+    x = x +2
+print 'Goodbye!'
