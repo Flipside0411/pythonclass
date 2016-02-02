d2bb713d111a2e248f76b6011292a2df8c54278f
Flipside0411 <hardypch@gmail.com>
2016-02-01 23:25:58 -0400

    New LectureCode files

--- runningtest.py
+++ runningtest.py
@@ -1,5 +1,12 @@
-num = 100
-while not False:
-    if num < 0:
-        break
-print 'num is: ' + str(num) 
+def lenIter(aStr):
+    '''
+    aStr: a string
+
+    returns: int, the length of aStr
+
+    '''
+    count=0
+
+    for char in aStr:
+       count +=1
+    return count
