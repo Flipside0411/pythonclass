420170c3b3b10e516d974f0ecdcef798eda754ab
Flipside0411 <hardypch@gmail.com>
2016-02-09 15:15:50 -0400

    updated files

---
+++ applyEachTo.py
@@ -0,0 +1,44 @@
+# noinspection PyUnreachableCode
+def applyEachTo(L, x):
+    result= []
+    for i in range(len(L)):
+        result.append(L[i](x))
+    return result
+
+
+
+def square(a):
+    return a*a
+
+
+def halve(a):
+    return a/2
+
+
+def inc(a):
+    return a+1
+
+
+applyEachTo([inc, square, halve, abs], -3)
+
+# noinspection PyUnreachableCode
+def applyEachTo(L, x):
+    result= []
+    for i in range(len(L)):
+        result.append(L[i](x))
+    return result
+
+
+def square(a):
+    return a*a
+
+
+def halve(a):
+    return a/2
+
+
+def inc(a):
+    return a+1
+
+
+applyEachTo([inc, square, halve, abs], -3)
--- runningtest.py
+++ runningtest.py
@@ -1,12 +1,8 @@
-def lenIter(aStr):
-    '''
-    aStr: a string
-
-    returns: int, the length of aStr
-
-    '''
-    count=0
-
-    for char in aStr:
-       count +=1
+def ndigits(x):
+    dig = str(x)
+    count =0
+    for i in dig:
+        count += 1
     return count
+
+print(ndigits(486))
